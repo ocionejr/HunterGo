5dcb72849f61ed9799b72b3bf4457ebe9fd7e779
VINICIUS_HIGUCHI <vinny.higuchi@gmail.com>
2020-07-12 00:06:42 -0300

    Tela Combate Finalizada

--- app/src/main/java/com/example/huntergo/CombateActivity.java
+++ app/src/main/java/com/example/huntergo/CombateActivity.java
@@ -8,15 +8,23 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.example.huntergo.CRUD.ArmaCRUD;
+import com.example.huntergo.CRUD.ArmaduraCRUD;
+import com.example.huntergo.CRUD.ItensEquipadorsCRUD;
 import com.example.huntergo.CRUD.JogadorCRUD;
+import com.example.huntergo.Classes.Arma;
+import com.example.huntergo.Classes.Armadura;
+import com.example.huntergo.Classes.ItensEquipados;
 import com.example.huntergo.Classes.Jogador;
 import com.example.huntergo.Classes.Monstro;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class CombateActivity extends AppCompatActivity {
@@ -31,6 +39,13 @@ public class CombateActivity extends AppCompatActivity {
     private ProgressBar mp_p;
     private ProgressBar hp_m;
     private int hp_monster;
+    private ItensEquipadorsCRUD itensCRUD;
+    private ItensEquipados itens;
+    private ArmaCRUD weaponCRUD;
+    private ArrayList<Arma> weapon;
+    private ArmaduraCRUD armorCRUD;
+    private ArrayList armor;
+    private Button spatk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +54,12 @@ public class CombateActivity extends AppCompatActivity {
 
         monstro = getIntent().getExtras().getParcelable("monstro");
         jogadorCRUD = JogadorCRUD.getINSTANCE();
+        itensCRUD = ItensEquipadorsCRUD.getINSTANCE();
+        itens = itensCRUD.getItensEquipados();
+        weaponCRUD = ArmaCRUD.getINSTANCE();
+        weapon = weaponCRUD.getArmas();
+        armorCRUD = ArmaduraCRUD.getINSTANCE();
+        armor = armorCRUD.getArmaduras();
         jogador = jogadorCRUD.getJogador();
         img_monstro = (ImageView) findViewById(R.id.img_monstro);
         evt = findViewById(R.id.evento);
@@ -59,6 +80,15 @@ public class CombateActivity extends AppCompatActivity {
                 Color.CYAN, android.graphics.PorterDuff.Mode.SRC_IN);
         mp_p.setProgress(jogador.getMana());
         mp_p.setScaleY(3f);
+        spatk = findViewById(R.id.spatk);
+
+        if(jogador.getClasse().equals("Guerreiro")){
+            spatk.setText("Golpe Brutal");
+        }else if(jogador.getClasse().equals("Caçador")){
+            spatk.setText("Flecha Vorpal");
+        }else if(jogador.getClasse().equals("Mago")){
+            spatk.setText("Bola de Fosgo");
+        }
 
         if(monstro.getNome().equals("Esqueleto")){
             img_monstro.setImageResource(R.drawable.esqueleto_combate);
@@ -81,9 +111,21 @@ public class CombateActivity extends AppCompatActivity {
 
     public void ataque(View v){
         int vi = monstro.getAtaque();
+        int danoArma = 0;
+        int danoArma2 = 0;
         evt.setText(" Atacou! +"+jogador.getAtaque()+"   -"+vi);
         if(jogador.getVelocidade() >= monstro.getVelocidade()){
-            hp_monster = hp_monster - jogador.getAtaque();
+            for(Arma armas : weapon){
+                if(Integer.parseInt(itens.getMaoDireita().toString()) == armas.getId()){
+                    danoArma = armas.getDano();
+                }
+            }
+            for(Arma armas : weapon){
+                if(Integer.parseInt(itens.getMaoEsquerda().toString()) == armas.getId()){
+                    danoArma2 = armas.getDano();
+                }
+            }
+            hp_monster = hp_monster - jogador.getAtaque() - danoArma - danoArma2;
             hp_m.setProgress(hp_monster);
             /*if(monstro.getVida() == 0){
 
@@ -97,6 +139,16 @@ public class CombateActivity extends AppCompatActivity {
             }*/
         }
         if(jogador.getVelocidade() < monstro.getVelocidade()){
+            for(Arma armas : weapon){
+                if(Integer.parseInt(itens.getMaoDireita().toString()) == armas.getId()){
+                    danoArma = armas.getDano();
+                }
+            }
+            for(Arma armas : weapon){
+                if(Integer.parseInt(itens.getMaoEsquerda().toString()) == armas.getId()){
+                    danoArma2 = armas.getDano();
+                }
+            }
             int vida = jogador.getVida() + (jogador.getDefesa() / 2);
             vida = vida - monstro.getAtaque();
             jogador.setVida(vida);
@@ -104,7 +156,7 @@ public class CombateActivity extends AppCompatActivity {
             /*if(jogador.getVida() == 0){
 
             }*/
-            hp_monster = hp_monster - jogador.getAtaque();
+            hp_monster = hp_monster - jogador.getAtaque() - danoArma - danoArma2;
             hp_m.setProgress(hp_monster);
             /*if(monstro.getVida() == 0){
 
@@ -123,8 +175,10 @@ public class CombateActivity extends AppCompatActivity {
         int vi = monstro.getAtaque();
         evt.setText(" Ataque especial! +"+jogador.getPodermagico()+"   -"+vi);
         if(jogador.getVelocidade() >= monstro.getVelocidade()){
-            hp_monster = hp_monster - jogador.getAtaque();
+            hp_monster = hp_monster - jogador.getPodermagico();
             hp_m.setProgress(hp_monster);
+            jogador.setMana(jogador.getMana() - 20);
+            mp_p.setProgress(jogador.getMana());
             /*if(monstro.getVida() == 0){
 
             }*/
@@ -146,7 +200,8 @@ public class CombateActivity extends AppCompatActivity {
             }*/
             hp_monster = hp_monster - jogador.getPodermagico();
             hp_m.setProgress(hp_monster);
-            int mana = jogador.getMana();
+            jogador.setMana(jogador.getMana() - 20);
+            mp_p.setProgress(jogador.getMana());
             /*if(monstro.getVida() == 0){
 
             }*/
